cfa81edeff8aba345f8be6095ea4f8c4c8978b69
= <victor.ferreira.silva001@gmail.com>
2023-09-05 08:27:07 -0300

    Att03ex01

---
+++ atividadesIndividuais/src/att03/ex01.java
@@ -0,0 +1,25 @@
+package att03;
+
+//Enunciado: 
+//Variáveis Privadas: Escreva uma classe Java com variáveis privadas representando o nome 
+//e a idade de uma pessoa. Forneça métodos públicos para definir e obter esses valores.
+
+public class ex01 {
+	private String nome;
+	private Integer idade;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+}
